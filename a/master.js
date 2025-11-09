36d83b6ffb470a08baf5843d58cbc0d3c12f472f
furusawakarina <168521603+furusawakarina@users.noreply.github.com>
2025-11-09 13:03:11 +0900

    Add files via upload

---
+++ a/master.js
@@ -0,0 +1,89 @@
+
+function onSongleAPIReady(Songle) {
+
+  // 音楽プレーヤーを表示する
+  var player = new Songle.Player({
+    mediaElement: document.querySelector('div.media'),
+    accessToken: '00000144-8PDPBsb', // 参加用トークン
+     secretToken: '5ruktp2XZhuB1rKBLhUTkTdq6b46bHDb'  // 認証用トークン
+	
+  });
+
+  player.addPlugin(new Songle.Plugin.SongleSync());
+
+  player.useMedia('https://youtu.be/9HKbo1FstOE?si=oQDYp79THG6JsZbb');
+
+
+//ビート
+player.addPlugin(new Songle.Plugin.Beat());
+
+//const tiles = document.querySelectorAll('#beats li');
+player.on("beatEnter",
+  function(ev) {
+    // 処理 ...
+	document.body.style.backgroundColor = "hsl(" + Math.random()*360 + ",90%,60%)";
+	
+  });
+
+//メロディ
+player.addPlugin(new Songle.Plugin.Melody());
+player.on("melodyEnter",
+  function(ev) {
+    // do someting ...
+	//document.body.style.backgroundColor = "hsl(" + Math.random()*360 + ",100%,60%)";
+  });
+
+//和音
+player.addPlugin(new Songle.Plugin.Chord());
+
+player.on("chordEnter",
+  function(ev) {
+    // do someting ...
+	//document.body.style.backgroundColor = "hsl(" + Math.random()*360 + ",90%,60%)";
+  });
+
+
+
+  // ページが読み込まれたら再生を開始する
+  player.on('mediaReady', function () {
+    player.play();
+  });
+
+  // 再生ボタンで再生を開始する
+  var playButton = document.querySelector('button.play');
+  playButton.addEventListener('click', function () {
+    player.play();
+  });
+
+//先頭
+  var seekToButton = document.querySelector('button.seekTo');
+  seekToButton.addEventListener('click', function () {
+    player.seekTo(0);
+  });
+//サビ
+
+
+  // 停止ボタンで再生を停止する
+  var pauseButton = document.querySelector('button.pause');
+  pauseButton.addEventListener('click', function () {
+    player.pause();
+  });
+
+//ビート・コード　プラグイン
+player.addPlugin(new Songle.Plugin.Beat());
+player.on("beatEnter",
+  function(ev) {
+    //  処理
+	
+  });
+player.addPlugin(new Songle.Plugin.Chord());
+
+
+  // 再生時刻を定期的に更新する
+  var span = document.querySelector('span.time');
+  setInterval(function () {
+    while (span.childNodes.length > 0) span.removeChild(span.childNodes[0]);
+    var textNode = document.createTextNode(parseInt(player.position));
+    span.appendChild(textNode);
+  }, 100);
+}
---
+++ a/slave.js
@@ -0,0 +1,62 @@
+function onSongleAPIReady(Songle){
+
+  var player = new Songle.Player({
+      accessToken: '00000144-8PDPBsb' // アクセストークン
+	//mediaElement: "#songle"
+  });
+
+  player.addPlugin(new Songle.Plugin.SongleSync());
+　//player.useMedia("https://www.youtube.com/watch?v=mTMs1S5td74");
+
+let syncMode = false;
+
+//和音
+player.addPlugin(new Songle.Plugin.Chord());
+
+player.on("chordEnter",
+  function(ev) {
+	if (!syncMode) return;
+     //do someting ...
+	//var synchronizeButton = document.querySelector('button.red');
+	document.body.style.backgroundColor = "hsl(" + Math.random()*360 + ",90%,60%)";
+	
+  });
+
+
+//同期ボタン
+const syncButton = document.querySelector('button.synchronize');
+  syncButton.addEventListener('click', () => {
+	
+	syncMode = true;
+	//player.addPlugin(new Songle.Plugin.Chord());
+
+    console.log("同期開始！");
+  });
+
+
+
+//停止
+const stopButton = document.querySelector('button.stop');
+  stopButton.addEventListener('click', () => {
+    document.body.style.backgroundColor ="white";
+　　syncMode = false;
+  });
+
+//ボタンでの切り替え
+
+
+
+const buttons = document.querySelectorAll('button[data-color]');
+
+buttons.forEach(btn => {
+  btn.addEventListener('click', () => {
+	syncMode = false;
+    const color = btn.dataset.color; // data-color属性を取る
+    document.body.style.backgroundColor = color;
+  });
+});
+
+
+
+  
+}
