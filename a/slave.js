38f6f4128b325e4a4eb7a953d210c30f13000151
furusawakarina <168521603+furusawakarina@users.noreply.github.com>
2025-12-09 01:45:39 +0900

    Update slave.js

--- a/slave.js
+++ a/slave.js
@@ -29,27 +29,62 @@ splayer.on("chordEnter",
      //do someting ...
 	const chordName = ev.data.chord.name;
 	let color = "white";
-		if (chordName.startsWith("C")){
-		color = "hsl(20,70%,60%)";
-		} else if (chordName.startsWith("G")) {
-		    color = "hsl(40, 70%, 60%)";
-		  } else if (chordName.startsWith("F")) {
-		    color = "hsl(55, 65%, 60%)";
-		  } else if (chordName.startsWith("Am")) {
-		    color = "hsl(220, 55%, 55%)";
-		  } else if (chordName.startsWith("Dm")) {
-		    color = "hsl(200, 50%, 50%)";
-		  } else if (chordName.startsWith("Em")) {
-		    color = "hsl(240, 60%, 50%)";
-		  }else if (chordName.startsWith("G7")) {
-		    color = "hsl(40, 85%, 55%)";
-		  }else if (chordName.startsWith("Cdim")) {
-		    color = "hsl(300, 95%, 40%)";
-		  }else if (chordName.startsWith("Csus4")) {
-		    color = "hsl(20, 95%, 60%)";
-		  }
+		if (chordName.startsWith("C"))
+		  h = 0;
+	  else if(chordName.startsWith("C#") || chordName.startsWith("Db"))
+		  h = 30;
+	  else if(chordName.startsWith("D"))
+		  h = 60;
+	  else if(chordName.startsWith("D#") || chordName.startsWith("Eb"))
+		  h = 90;
+	  else if(chordName.startsWith("E"))
+		  h = 120;
+	  else if(chordName.startsWith("F"))
+		  h = 150;
+	  else if(chordName.startsWith("F#") || chordName.startsWith("Gb"))
+		  h = 180;
+	  else if(chordName.startsWith("G"))
+		  h = 210;
+	  else if(chordName.startsWith("G#") || chordName.startsWith("Ab"))
+		  h = 240;
+	  else if(chordName.startsWith("A"))
+		  h = 270;
+	  else if(chordName.startsWith("A#") || chordName.startsWith("Bb"))
+		  h = 300;
+	  else if(chordName.startsWith("B"))
+		  h = 330;
 	  
-	document.body.style.backgroundColor = color;
+	let s = 65;
+	  if (chordName.includes("6"))
+		  s = 68;
+	  else if(chordName.includes("maj7"))
+		  s = 72;
+	  else if(chordName.includes("m7"))
+		  s = 68;
+	  else if(chordName.includes("m7(♭5)"))
+		  s = 82;
+	  else if(chordName.includes("7"))
+		  s = 80;
+	  else if(chordName.includes("m"))
+		  s = 60;
+	  else if(chordName.includes("dim"))
+		  s = 88;
+	  else if(chordName.includes("aug"))
+		  s = 78;
+
+	  let l = 50;
+		  if(chordName.includes("/3"))
+			  l = 75;
+		  else if(chordName.includes("/5"))
+			l = 68;
+		  else if (chordName.includes("/2"))
+			  l = 65;
+		  else if (chordName.includes("/♭7"))
+			  l = 60;
+		  else if (chordName.includes("/7"))
+			  l = 55;
+
+	  document.body.style.backgroundColor = `hsl(${h}, ${s}%, ${l}%)`;
 	
   });
 
